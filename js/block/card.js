88388a3e9d19be08ef64dd68bf8a7f234dc31c13
luanerdy <luan.vini1@gmail.com>
2020-04-05 02:42:34 -0300

    adicionado o botão de excluir

--- js/block/card.js
+++ js/block/card.js
@@ -1,43 +1,12 @@
-/*var $cardColors = document.querySelectorAll('.card-colors');
-var $cards = document.querySelectorAll('.card');
-
-for (var index = 0; index < $cardColors.length; index++) {
-
-	$cardColors[index].addEventListener('click', trocaCor, true);
-
-}
-
-for (var index = 0; index < $cards.length; index++) {
-
-	$cards[index].addEventListener('click', removeActive, true);
-
-}
-
-function trocaCor() {
-
-	var $card = this.parentNode.parentNode.parentNode;
-	$card.dataset.color = this.dataset.color;
-
-	this.classList.add('isActive');
-
-}
-
-function removeActive() {
-	let balls = this.children[0].children[0].children;
-	for(var k = 0; k < balls.length; k++){
-		balls[k].classList.remove('isActive');
-	}
-}*/
 
 var $cards = document.querySelectorAll('.card');
 
 for (var i = 0; i < $cards.length; i++) {
 	$cards[i].addEventListener('click', function(e) {
 		var $this = e.target;
+		var $card = this;
 
-		if($this.classList.contains('card-colors')) {
-
-			var $card = this;
+		if($this.dataset.color) {
 
 			for (var i = 0; i < $this.parentNode.children.length; i++) {
 				$this.parentNode.children[i].classList.remove('isActive');
@@ -47,6 +16,10 @@ for (var i = 0; i < $cards.length; i++) {
 
 			$card.dataset.color = $this.dataset.color;
 
+		}
+
+		if($this.classList.contains('card_delete')) {
+			$card.remove();
 		}
 	});
 }
