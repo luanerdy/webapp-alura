879103ae14320feb7ea87ab4b83f5025a47d28d9
luanerdy <luan.vini1@gmail.com>
2020-04-06 00:31:16 -0300

    adicionando botão de edita

--- js/block/card.js
+++ js/block/card.js
@@ -5,6 +5,7 @@ for (var i = 0; i < $cards.length; i++) {
 	$cards[i].addEventListener('click', function(e) {
 		var $this = e.target;
 		var $card = this;
+		var $cardContent = $card.querySelector('.card-content');
 
 		if($this.dataset.color) {
 
@@ -20,6 +21,19 @@ for (var i = 0; i < $cards.length; i++) {
 
 		if($this.classList.contains('card_delete')) {
 			$card.remove();
+		}
+
+		if($this.classList.contains('card_edit')){
+			if($cardContent.getAttribute('contenteditable') === 'false'){
+				$cardContent.setAttribute('contenteditable', 'true');
+				$cardContent.focus();
+				$this.classList.add('isActive');
+			}
+			else {
+				$cardContent.setAttribute('contenteditable', 'false');
+				$cardContent.blur();
+				$this.classList.remove('isActive');
+			}
 		}
 	});
 }
